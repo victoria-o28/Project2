96723440bfe4d796c5bc08c1dd733743d88de98f
Victoria <vickkeyodhiambo5@gmail.com>
2022-03-06 10:36:49 +0300

    Create arrays for males and females with their day name

---
+++ index.js
@@ -0,0 +1,60 @@
+const males = [{
+        day: 'Sunday',
+        name: 'Kwasi'
+    },
+    {
+        day: 'Monday',
+        name: 'Kwadwo'
+    },
+    {
+        day: 'Tuesday',
+        name: 'Kwabena'
+    },
+    {
+        day: 'Wednesday',
+        name: 'Kwaku'
+    },
+    {
+        day: 'Thursday',
+        name: 'Yaw'
+    },
+    {
+        day: 'Friday',
+        name: 'Kofi'
+    },
+    {
+        day: 'Saturday',
+        name: 'Kwame'
+    },
+];
+
+const females = [{
+        day: 'Sunday',
+        name: 'Akosua'
+    },
+    {
+        day: 'Monday',
+        name: 'Adwoa'
+    },
+    {
+        day: 'Tuesday',
+        name: 'Abenaa'
+    },
+    {
+        day: 'Wednesday',
+        name: 'Akua'
+    },
+    {
+        day: 'Thursday',
+        name: 'Yaa'
+    },
+    {
+        day: 'Friday',
+        name: 'Afua'
+    },
+    {
+        day: 'Saturday',
+        name: 'Ama'
+    },
+];
+
